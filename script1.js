92d7e900f6f9e4065351c2bb4ef67abf23c63a5c
HariPrasath12051997 <122340623+HariPrasath12051997@users.noreply.github.com>
2023-02-26 19:41:52 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,15 @@
+
+var request=new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v3.1/all");
+request.send();
+request.onload=function(){
+var result=JSON.parse(request.response);
+for (i=0;i<result.length;i++){
+    console.log("& flag :",result[i].flag);
+    // console.log("County:",result[i].name,"& Region:",result[i].region,"& Sub-Region:",result[i].subregion,"&Population:",result[i].population);
+// console.log("County:",result[i].name,"& flag :",result[i].flag);
+// // }
+// // }
+//   console.log(result);
+
+}}  
